6a3952cb3f5ce5f066121338b619c1d5f1368571
yourdesignerfriends <del23038@byui.edu>
2024-11-08 01:17:47 -0800

    fix code some decor flowers

---
+++ scripts/getdates.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const currentYear = new Date().getFullYear();
+    const lastModified = document.lastModified;
+    const author = "Analina Del Vecchio"; 
+    const place = "Washington, USA";
+    const copyrightElement = document.getElementById("copyright");
+    
+    if (copyrightElement) { 
+        copyrightElement.innerHTML = `\u00A9 ${currentYear} 🌺 ${author} 🌺 ${place}`;
+    }
+    const modifiedElement = document.getElementById("lastModified");
+    if (modifiedElement) {
+        modifiedElement.innerHTML = `Last Modified: ${lastModified}`;
+    }
+    });

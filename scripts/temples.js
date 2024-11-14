dd82862627912134c0b1b9b33d4141178de83086
yourdesignerfriends <del23038@byui.edu>
2024-11-13 16:41:05 -0800

    Picture album in progresss....

---
+++ scripts/temples.js
@@ -0,0 +1,36 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const container = document.getElementById('temple-container');
+    const templeData = [
+        {
+            src: "images/seattle-washington-temple.webp",
+            alt: "Seattle Washington Temple",
+            caption: "Seattle Washington Temple",
+        },
+    ];
+    // I'll use a for loop to repeat the image 10 times.
+    for (let i = 0; i < 10; i++) {
+        templeData.forEach(data => {
+            const figure = document.createElement('figure');
+            const img = document.createElement('img');
+            img.src = data.src;
+            img.alt = data.alt;
+
+            const figcaption = document.createElement('figcaption');
+            figcaption.textContent = data.caption;
+
+            figure.appendChild(img);
+            figure.appendChild(figcaption);
+            container.appendChild(figure);
+        });
+    }
+
+    const menuLinks = document.querySelectorAll('.menu-link');
+    const mainHeading = document.getElementById('main-heading');
+
+    menuLinks.forEach(link => {
+        link.addEventListener('click', (event) => {
+        event.preventDefault();
+        mainHeading.textContent = event.target.textContent;
+        });
+    });
+});

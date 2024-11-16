69c7330caa2aa2865a85a38f8da5656d1b095f2b
yourdesignerfriends <del23038@byui.edu>
2024-11-15 20:33:12 -0800

    some changes in js

--- scripts/temples.js
+++ scripts/temples.js
@@ -13,9 +13,11 @@ document.addEventListener('DOMContentLoaded', () => {
     /* ------------- hamburger menu button -------------*/
     const hamButton = document.querySelector('#menu');
     const navigation = document.querySelector('.navigation');
+    const mainTitle = document.querySelector('#main-title');
 
     hamButton.addEventListener('click', () => {
         navigation.classList.toggle('open');
         hamButton.classList.toggle('open');
+        mainTitle.classList.toggle('hidden');
     });
 });

d048da84a24d4cd101c73327a3a9142c8fd0d315
yourdesignerfriends <del23038@byui.edu>
2024-11-23 21:45:21 -0800

    add temples

--- scripts/filtered-temples.js
+++ scripts/filtered-temples.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
-    /* ------------- Navigation -------------*/
+    /* Navigation */
     const menuLinks = document.querySelectorAll('.menu-link');
     const mainHeading = document.getElementById('main-heading');
 
@@ -9,8 +9,7 @@ document.addEventListener('DOMContentLoaded', () => {
             mainHeading.textContent = event.target.textContent;
         });
     });
-
-    /* ------------- hamburger menu button -------------*/
+    /* hamburger menu button */
     const hamButton = document.querySelector('#menu');
     const navigation = document.querySelector('.navigation');
     const mainTitle = document.querySelector('#main-title');
@@ -20,4 +19,87 @@ document.addEventListener('DOMContentLoaded', () => {
         hamButton.classList.toggle('open');
         mainTitle.classList.toggle('hidden');
     });
+    /* Temples */
+    const temples = [
+        {
+          templeName: "Aba Nigeria",
+          location: "Aba, Nigeria",
+          dedicated: "2005, August, 7",
+          area: 11500,
+          imageUrl:
+          "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/aba-nigeria/400x250/aba-nigeria-temple-lds-273999-wallpaper.jpg"
+        },
+        {
+          templeName: "Manti Utah",
+          location: "Manti, Utah, United States",
+          dedicated: "1888, May, 21",
+          area: 74792,
+          imageUrl:
+          "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/manti-utah/400x250/manti-temple-768192-wallpaper.jpg"
+        },
+        {
+          templeName: "Payson Utah",
+          location: "Payson, Utah, United States",
+          dedicated: "2015, June, 7",
+          area: 96630,
+          imageUrl:
+          "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/payson-utah/400x225/payson-utah-temple-exterior-1416671-wallpaper.jpg"
+        },
+        {
+          templeName: "Yigo Guam",
+          location: "Yigo, Guam",
+          dedicated: "2020, May, 2",
+          area: 6861,
+          imageUrl:
+          "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/yigo-guam/400x250/yigo_guam_temple_2.jpg"
+        },
+        {
+          templeName: "Washington D.C.",
+          location: "Kensington, Maryland, United States",
+          dedicated: "1974, November, 19",
+          area: 156558,
+          imageUrl:
+          "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/washington-dc/400x250/washington_dc_temple-exterior-2.jpeg"
+        },
+        {
+          templeName: "Lima Perú",
+          location: "Lima, Perú",
+          dedicated: "1986, January, 10",
+          area: 9600,
+          imageUrl:
+          "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/lima-peru/400x250/lima-peru-temple-evening-1075606-wallpaper.jpg"
+        },
+        {
+          templeName: "Mexico City Mexico",
+          location: "Mexico City, Mexico",
+          dedicated: "1983, December, 2",
+          area: 116642,
+          imageUrl:
+          "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/mexico-city-mexico/400x250/mexico-city-temple-exterior-1518361-wallpaper.jpg"
+        },
+        {
+            templeName: "Rome Italy Temple",
+            location: "Rome, Italy",
+            dedicated: "2019, March, 10",
+            area: 41010,
+            imageUrl:
+            "https://churchofjesuschristtemples.org/assets/img/temples/rome-italy-temple/rome-italy-temple-3549.jpg"
+        },
+        {
+            templeName: "Caracas Venezuela Temple",
+            location: "Caracas, Venezuela",
+            dedicated: "2000, August, 5",
+            area: 15332,
+            imageUrl:
+            "https://churchofjesuschristtemples.org/assets/img/temples/_temp/096-Caracas-Venezuela-Temple.jpg"
+        },
+        {
+            templeName: "Hong Kong China Temple",
+            location: "Hong Kong, China",
+            dedicated: "1996, May, 26",
+            area: 51921,
+            imageUrl:
+            "https://churchofjesuschristtemples.org/assets/img/temples/hong-kong-china-temple/hong-kong-china-temple-53443.jpg"
+        },
+    ];
 });

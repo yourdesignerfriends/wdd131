c318279f125df4532e01dda9239523ee84de15e1
yourdesignerfriends <del23038@byui.edu>
2024-12-09 21:21:22 -0800

    imporve js

--- scripts/review.js
+++ scripts/review.js
@@ -1,4 +1,20 @@
 document.addEventListener('DOMContentLoaded', () => {
+    // Last modified 
+    const currentYear = new Date().getFullYear(); 
+    const lastModified = document.lastModified; 
+    const author = "Analina Del Vecchio Madriz";
+    const place = "Washington, USA";
+
+    const copyrightElement = document.getElementById("copyright"); 
+    const modifiedElement = document.getElementById("lastModified");
+    
+    if (copyrightElement) { 
+        copyrightElement.textContent = `\u00A9 ${currentYear} | ${author} | ${place}`; 
+        } 
+    if (modifiedElement) { 
+        modifiedElement.textContent = `Last modification: ${lastModified}`; 
+    }
+    // Adding a counter
     let reviewsCompleted = parseInt(localStorage.getItem('reviewsCompleted')) || 0;
 
     reviewsCompleted++;

6624cad73dabf0f6d4a52ea31139a7efcafd3626
yourdesignerfriends <del23038@byui.edu>
2024-12-11 20:40:36 -0800

    create fuction

--- project/scripts/array-lazy-loading.js
+++ project/scripts/array-lazy-loading.js
@@ -1,6 +1,28 @@
 document.addEventListener('DOMContentLoaded', () => {
-// Projects Array
+    // Projects Array
+    const projects = [
+        {
+            projectName: "Tribal Woven Baskets",
+            yearOfCreation: "2024",
+            projectDescription: "Local online store based in USA where home decoration products are sold.",
+            imageUrl: "images/tribal-woven-baskets.webp",
+            link: "https://yourdesignerfriends.com/",
+        }, 
+        {
+            projectName: "Your Designer Friends",
+            yearOfCreation: "2021",
+            projectDescription: "Company dedicated to providing brand design, tutoring and software development services.",
+            imageUrl: "images/your-designer-friends.webp",
+            link: "https://www.tribalwovenbaskets.com/",
+        },  
+    ];
+    // Function to create project cards
+    function createProjectCard(project) {
+        let card = document.createElement("section");
+        card.classList.add("project-card");
 
-}); 
-
-
+        let name = document.createElement("h3");
+        let link = document.createElement("a");
+        link.setAttribute("href", project.link);
+        link.setAttribute("target", "_blank");
+});

114bf0e9c44730138478557b674f050ee248d297
yourdesignerfriends <del23038@byui.edu>
2024-12-12 21:32:01 -0800

    change js

--- project/scripts/project.js
+++ project/scripts/project.js
@@ -86,7 +86,8 @@ document.addEventListener('DOMContentLoaded', () => {
         img.setAttribute("src", project.imageUrl);
         img.setAttribute("alt", `${project.projectName} Image`);
         img.setAttribute("loading", "lazy");
-
+        img.setAttribute("width", "380");
+        img.setAttribute("height", "356");
 
         card.appendChild(name);
         card.appendChild(year);

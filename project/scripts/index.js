f604966ef3d65f7f5c2b9bb25cd0e717904958a6
yourdesignerfriends <del23038@byui.edu>
2024-12-13 18:58:40 -0800

    separate mi js to avoid errors

--- project/scripts/index.js
+++ project/scripts/index.js
@@ -1,41 +1,4 @@
 document.addEventListener('DOMContentLoaded', () => {
-    // Navigation
-    const menuLinks = document.querySelectorAll('.menu-link');
-    const mainHeading = document.getElementById('heading-h1');
-
-    menuLinks.forEach(link => {
-        link.addEventListener('click', (event) => {
-            const filter = event.target.textContent.toLowerCase();
-            mainHeading.textContent = event.target.textContent;
-        });
-    });
-    // Hamburger menu button
-    const hamButton = document.querySelector('#menu');
-    const navigation = document.querySelector('.navigation');
-    const mainTitle = document.querySelector('#current-page');
-
-    hamButton.addEventListener('click', () => {
-        navigation.classList.toggle('open');
-        hamButton.classList.toggle('open');
-        mainTitle.classList.toggle('hidden');
-    });
-
-    // footer 
-    const currentYear = new Date().getFullYear(); 
-    const lastModified = document.lastModified; 
-    const author = "Analina Del Vecchio Madriz";
-    const place = "Washington, USA";
-
-    const copyrightElement = document.getElementById("copyright"); 
-    const modifiedElement = document.getElementById("lastModified");
-    
-    if (copyrightElement) { 
-        copyrightElement.textContent = `\u00A9 ${currentYear} ${author} ${place}`; 
-        } 
-    if (modifiedElement) { 
-        modifiedElement.textContent = `Last modification: ${lastModified}`; 
-    } 
-
     function preloadImage(url) {
         const link = document.createElement('link');
         link.rel = 'preload';
